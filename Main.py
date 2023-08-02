179ffec06caa0ff4b3cc3c2bfdea197cd8f807b3
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2023-08-03 00:17:04 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -122,7 +122,7 @@ try:
                         final_response = message_placeholder.markdown(full_response + "▌")
                         time.sleep(0.15)
                     content = response.choices[0].delta.get("content", "")
-                    if not content:
+                    if not full_response:
                         create_message()
                 create_message()
                 st.session_state["last_received_time"] = time.time()
