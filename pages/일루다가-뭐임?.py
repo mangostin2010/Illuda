344d65e7f95473b178faa3b57cd7a93f28fe3868
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2023-08-02 20:12:45 +0900

    Update 일루다가-뭐임?.py

--- pages/일루다가-뭐임?.py
+++ pages/일루다가-뭐임?.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
+#st.set_page_config(page_title='AI Tools', page_icon='🤖', initial_sidebar_state="expanded")
+st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
+
+
 st.set_page_config(
     page_title="일루다가 뭐임?",
     page_icon="👧",
