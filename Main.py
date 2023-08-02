a37304566aadb290765215e7c20d398b9cd3140f
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2023-08-02 19:59:39 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -120,5 +120,5 @@ try:
             st.session_state.messages.append({"role": "assistant", "content": full_response})
             
 
-except:
+except Filesdfaasdf:
     st.success('이 페이지를 새로고침 해주세요.')
